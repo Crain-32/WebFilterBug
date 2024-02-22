5d0fc24b1ecf0f4371ae88296b82c9c3f16e5716
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2024-02-22 14:56:06 -0700

    Bug Commit

--- src/main/java/dev/crain/webfilterbug/WebfilterBugApplication.java
+++ src/main/java/dev/crain/webfilterbug/WebfilterBugApplication.java
@@ -1,13 +1,71 @@
 package dev.crain.webfilterbug;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class WebfilterBugApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebfilterBugApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebfilterBugApplication.class, args);
+    }
 
+    @WebFilter("/not/simple")
+    @Component
+    public static class SimpleFilterOne extends OncePerRequestFilter {
+
+        public static boolean CALLED = false;
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            CALLED = true;
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @WebFilter(value = "/not/simple")
+    @Component
+    public static class SimpleFilterTwo extends OncePerRequestFilter {
+
+        public static boolean CALLED = false;
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            CALLED = true;
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @WebFilter(urlPatterns = "/not/simple")
+    @Component
+    public static class SimpleFilterThree extends OncePerRequestFilter {
+
+        public static boolean CALLED = false;
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            CALLED = true;
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @RestController
+    @RequestMapping("/rest/simple")
+    public static class SimpleController {
+
+
+        @GetMapping
+        public String getString() {
+            return "Hello World";
+        }
+    }
 }
--- src/test/java/dev/crain/webfilterbug/WebfilterBugApplicationTests.java
+++ src/test/java/dev/crain/webfilterbug/WebfilterBugApplicationTests.java
@@ -1,13 +1,36 @@
 package dev.crain.webfilterbug;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebfilterBugApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void itShould_NotCallTheFilters() throws Exception {
+        WebfilterBugApplication.SimpleFilterOne.CALLED = false;
+        WebfilterBugApplication.SimpleFilterTwo.CALLED = false;
+        WebfilterBugApplication.SimpleFilterThree.CALLED = false;
+        mockMvc.perform(get("/rest/simple"))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes("Hello World".getBytes()));
+        Assertions.assertAll(
+                () -> Assertions.assertFalse(WebfilterBugApplication.SimpleFilterOne.CALLED),
+                () -> Assertions.assertFalse(WebfilterBugApplication.SimpleFilterTwo.CALLED),
+                () -> Assertions.assertFalse(WebfilterBugApplication.SimpleFilterThree.CALLED)
+        );
+    }
 
 }
