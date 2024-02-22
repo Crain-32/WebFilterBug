b895dcff428dca2dbd9ae52804d3c205fc4265e2
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2024-02-22 14:53:46 -0700

    Initial Commit

---
+++ src/main/java/dev/crain/webfilterbug/WebfilterBugApplication.java
@@ -0,0 +1,13 @@
+package dev.crain.webfilterbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfilterBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfilterBugApplication.class, args);
+	}
+
+}
